2082e9f0a8c1f629ed7648fad3e26d6d2177e74e
Dkriss <89033170+Dkriss@users.noreply.github.com>
2023-12-10 23:16:11 +0700

    ngerapihin

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
+// MENGAMBIL DARI DATABASE INFO DI DBCONNECTION
 const dbConnection = require('./model/dbConnection');
 const userRouter = require('./routes/signup');
 
@@ -12,6 +13,11 @@ app.use(cors());
 app.use('/registration', userRouter); // Gunakan router secara langsung
 app.use('/login', userRouter); // Gunakan router secara langsung
 
+// const formidableMiddleware = require('express-formidable');
+// gunakan middleware formidable pada aplikasi express Anda
+// app.use(formidableMiddleware());
+
+// UNTUK MENGUJI DENGAN TRY CATCH APAKAH SUDAH TERHUBUNG ATAU TIDAK KE DB
 const port = 3000;
 app.listen(port, () => {
     console.log('App listening on port 3000');
