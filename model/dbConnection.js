3a692691074bfb6cfd950ea34fd7060b3c37042a
Setianto313 <setianto354@gmail.com>
2023-12-12 13:41:02 +0700

    UPDATE DIKIT

--- model/dbConnection.js
+++ model/dbConnection.js
@@ -1,23 +1,12 @@
-// const sequelize = require('sequelize');
-// const mysql = require('mysql2');
-// var db = new sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_password);
-// dotenv.config();
-
-// const dbConection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//     host: process.env.DB_HOST,
-//     dialect: process.env.DB_DIALECT,
-// });
-
-
-// module.exports = dbConection;
 const Sequelize = require('sequelize');
 const dotenv = require('dotenv');
 
-dotenv.config(); // Load environment variables from .env file
+dotenv.config();
 
 const dbConnection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT
 });
 
 module.exports = dbConnection;
