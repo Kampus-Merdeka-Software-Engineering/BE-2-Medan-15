29e759fe0adcb51862dd7541a2e8509c9d21dcff
Setianto313 <setianto354@gmail.com>
2023-12-14 22:23:34 +0700

    update

--- model/dbConnection.js
+++ model/dbConnection.js
@@ -5,7 +5,8 @@ dotenv.config();
 
 const dbConnection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT
 });
 
 module.exports = dbConnection;
